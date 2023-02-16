ee3abd7eda95b3e32975d4fd7a36119654bbfca1
lewys-miugo <lewiswambugu01@gmail.com>
2023-02-16 15:26:35 +0300

    adding flowbite mods

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('flowbite/plugin')
+    ],
 };
