e2ef66aa78c13818a0dc602243896427937337cf
lewys-miugo <lewiswambugu01@gmail.com>
2023-02-20 12:36:54 +0300

    adding banner

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+@section('content')
+<section>
+    <!-- Banner -->
+    <div class="grid max-w-screen-xl px-4 pt-8 mx-auto lg:gap-8 xl:gap-0 lg:pt-2 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">About <br> Prof  Moses  Ogutu</h1>
+            <p class="max-w-2xl mb-6 text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-600">Prof Moses Ogutu is a public servant currently serving as the Kenya’s Ambassador to the Kingdom of Belgium, Professor of Entrepreneurship, Technocrat, previously the Permanent Secretary of Kenya’s Ministry of Information and Communication, editor, advisor and a champion of Pan-Africanism.</p>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Read Full Profile
+            </a> 
+            <!-- <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a> -->
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg bg-blue-500 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Download Printable Profile
+            </a> 
+        </div>
+        <div class=" lg:col-span-5 lg:flex">
+            <img src="{{url('/images/profileimg-removebg-preview.png')}}" alt="mockup">
+        </div>                
+    </div>
+
+    <!-- Profile key experience -->
+    
+</section>
+@endsection
