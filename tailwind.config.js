0f755dad3aa15ea2c0a63ae83db29f94d149605f
lewys-miugo <lewiswambugu01@gmail.com>
2023-03-03 01:41:19 +0300

    Adding customized fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
+      colors:{
+        bluetange:'#00063D'
+      },
+      fontSize:{
+        sitange:['20px','30px'],
+        sitangeh:['52px','52px']
+
+      },
+      fontFamily:{
+        sanx:['Merriweather\\ Sans','sans-serif']
+      },
       backgroundImage:{
             'pattern-1':"url('../public/images/background_pattern1.png')", 
           },
