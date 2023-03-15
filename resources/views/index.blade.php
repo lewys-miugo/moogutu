8a08ab8d77cc89ee5f6e6118473c837b0206732d
lewys-miugo <lewiswambugu01@gmail.com>
2023-03-15 23:02:57 +0300

    cards palced in order

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,10 +17,6 @@
                             <a href="{{'about'}}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-semibold text-center text-bluetange border border-gray-300 rounded-lg bg-orangelink hover:bg-gray-100 focus:ring-4 focus:ring-blue-500">
                                 About Moses Ogutu
                             </a> 
-                            <!-- <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                                Get started
-                                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a> -->
                             <a href="{{'blog'}}" class="inline-flex items-center justify-center px-5 py-3 text-base font-semibold text-center text-bluetange border border-gray-300 rounded-lg bg-orangelink hover:bg-gray-100 focus:ring-4 focus:ring-blue-500">
                                 Blogs & Publication
                             </a> 
@@ -36,7 +32,7 @@
 
             <ul class=" lg:mx-16  text-base font-medium text-center text-gray-900 divide-x divide-gray-200 rounded-lg shadow flex flex-row sm:flex  ">
                 <li class="w-full">
-                    <a href="#" class="inline-block  w-full p-4 text-white bg-bluetange text-lg hover:text-orangelink  rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
+                    <a href="{{'profile'}}" class="inline-block  w-full p-4 text-white bg-bluetange text-lg hover:text-orangelink  rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
                 </li>
                 <li class="w-full">
                     <a href="#" class="inline-block w-full p-4 text-white bg-bluetange text-lg hover:text-orangelink  focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Academia</a>
@@ -54,22 +50,6 @@
         <img src="/images/background_pattern1.png" alt="" class=" w-full h-min invisible lg:visible">
 
     </div>
-    <!-- navigation Tabs -->
-
-    <!-- <ul class="lg:px-16 bg-white text-base font-medium text-center text-gray-900 divide-x divide-gray-200 rounded-lg shadow flex flex-row sm:flex  dark:divide-gray-700 dark:text-gray-400">
-        <li class="w-full">
-            <a href="#" class="inline-block  w-full p-4 text-white bg-blue-800 hover:text-black hover:bg-blue-500  rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
-        </li>
-        <li class="w-full">
-            <a href="#" class="inline-block w-full p-4 text-white bg-blue-800 hover:text-black hover:bg-blue-500  focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Academia</a>
-        </li>
-        <li class="w-full">
-            <a href="#" class="inline-block w-full p-4 text-white bg-blue-800 hover:text-black hover:bg-blue-500  focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Reflections</a>
-        </li>
-        <li class="w-full">
-            <a href="#" class="inline-block w-full p-4 text-white bg-blue-800 hover:text-black hover:bg-blue-500 rounded-r-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Gallery</a>
-        </li>
-    </ul> -->
 
     <!-- Biography -->
     <div class="lg:mx-12 lg:mt-14">
